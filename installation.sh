20ee9b626141e07b10489c7dbab8875710a19aa3
Kenny Perquy <perquy.k@gmail.com>
2026-01-15 03:39:11 +0100

    added more packages and git config

--- installation.sh
+++ installation.sh
@@ -2,8 +2,31 @@
 
 echo "Starting Kenny's installation script..."
 
-packages=(
-  
-  htop
+username="Kenny Perquy"
+usermail="perquy.k@gmail.com"
 
+packages=(
+  neovim
+  curl
+  git
+  github-cli
+  btop
+  fastfetch
+  waybar
+  hyprpaper
+  rofi
 )
+
+echo "Installing packages..."
+for package in ${packages[@]}; do
+  yay -S ${package} --noconfirm --needed
+done
+
+echo "Setting up git with following settings:" 
+echo "name: ${username}"
+echo "email: ${email}"
+echo "default branch: main"
+git config set --global init.defaultBranch main
+git config set --global user.name ${username}
+git config set --global user.email ${email}
+
